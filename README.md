75b9a9756d1eff48383e588c65182986616b9990
Paul M <paulm@ensemble.com>
2023-01-15 09:58:45 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sample-node-app
+# basic app
+The app works when you get the confirmation message in console output of npm run test
+
+## requirements:
+node & postgres
+
+## How To
+Duplicate `.env.sample` and insert the required values for your setup
+Run `npm run test`
---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client } = require('pg')
+require('dotenv').config();
+
+const client = new Client({
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: false,
+})
+
+execute()
+
+async function execute() {
+    try {
+        await client.connect()
+        console.log("Connected to db")
+        console.log("If you see this message the project runs as intended, good job!")
+    }
+    catch (ex) {
+        console.log(`Something is not quite right ${ex}`)
+    }
+    finally {
+        await client.end()
+        console.log("Client disconnected")
+    }
+
+}
