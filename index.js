9c21f31fbcf2aec307bea2c05350eb674bdee996
Paul M <paulm@ensemble.com>
2023-01-15 10:04:56 +0200

    1st test subject

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Client({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
-    ssl: false,
+    ssl: true,
 })
 
 execute()
